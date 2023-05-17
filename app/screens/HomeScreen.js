a0c71cc415a628e468b02a9c90394fb9da42b7d8
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-17 15:29:21 +0300

    build the header in the home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,7 +1,8 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useLayoutEffect } from "react";
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, Image } from "react-native";
 import Constants from "expo-constants";
+import { EvilIcons, Entypo } from "@expo/vector-icons";
 
 function HomeScreen(props) {
   const navigation = useNavigation();
@@ -14,15 +15,51 @@ function HomeScreen(props) {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home screen </Text>
+      <View style={styles.header}>
+        <View style={styles.detailsContainer}>
+          <Image style={styles.image} source={require("../assets/del.jpg")} />
+          <View>
+            <Text style={styles.deliver}>Deliver now</Text>
+            <Text style={styles.location}>
+              Current Location{" "}
+              <Entypo color="#00ccbb" name="chevron-down" size={15} />
+            </Text>
+          </View>
+        </View>
+        <EvilIcons name="user" size={40} color="#00ccbb" />
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ffff",
+    padding: 5,
+    paddingTop: Constants.statusBarHeight + 10,
+  },
+  detailsContainer: {
+    flexDirection: "row",
+  },
+  deliver: {
+    fontWeight: "bold",
+    opacity: 0.5,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    width: 40,
+    height: 40,
+    resizeMode: "cover",
+    borderRadius: 20,
+    marginRight: 8,
+  },
+  location: {
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
 
