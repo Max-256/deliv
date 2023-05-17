6f139fac0f258cc8472fa37db83cd495dad78083
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-17 10:57:00 +0300

    add stack navigation

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
 import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
+    <>
       <StatusBar style="dark" backgroundColor="white" />
-      <HomeScreen />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
