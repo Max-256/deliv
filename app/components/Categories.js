f2348e55904d7cc2e10fa23416c1ce6156460f9d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-05-17 21:58:49 +0300

    improve the category card component

--- app/components/Categories.js
+++ app/components/Categories.js
@@ -1,11 +1,38 @@
 import React from "react";
 import { ScrollView, View, StyleSheet, Text } from "react-native";
+
 import CategoryCard from "./CategoryCard";
 
 function Categories(props) {
   return (
-    <ScrollView horizontal style={styles.container}>
-      <CategoryCard />
+    <ScrollView
+      contentContainerStyle={{
+        paddingTop: 5,
+      }}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      style={styles.container}
+    >
+      <CategoryCard
+        imageUrl="https://links.papareact.com/wru"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://links.papareact.com/wru"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://links.papareact.com/wru"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://links.papareact.com/wru"
+        title="Testing"
+      />
+      <CategoryCard
+        imageUrl="https://links.papareact.com/wru"
+        title="Testing"
+      />
     </ScrollView>
   );
 }
--- app/components/CategoryCard.js
+++ app/components/CategoryCard.js
@@ -1,16 +1,34 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
 
-function CategoryCard(props) {
+function CategoryCard({ imageUrl, title }) {
   return (
-    <View style={styles.container}>
-      <Text>Category card</Text>
-    </View>
+    <TouchableOpacity style={styles.container}>
+      <Image source={{ uri: imageUrl }} style={styles.image} />
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    backgroundColor: "gray",
+    borderRadius: 3,
+    position: "relative",
+    marginRight: 5,
+    overflow: "hidden",
+  },
+  image: {
+    height: 75,
+    width: 75,
+  },
+  text: {
+    position: "absolute",
+    bottom: 1,
+    fontWeight: "bold",
+    left: 5,
+    color: "#fff",
+  },
 });
 
 export default CategoryCard;
